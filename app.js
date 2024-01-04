424118be7bd0dfc6b3b4d2a3b510b5d562940357
Deepak Gholave <153365390+deepakgholave@users.noreply.github.com>
2024-01-04 14:57:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express'),
     // Mitigate XSS using sanitizer
     sanitizer = require('sanitizer'),
     app = express(),
-    port = 8000
+    port = 800
 
 app.use(bodyParser.urlencoded({
     extended: false
