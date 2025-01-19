8808c0699416e3ddbf7b797890244ee6ac98a53b
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-19 22:23:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # pulling docker images
-docker pull amitabhdevops/node-todo-app:v1
+docker pull amitabhdevops/aws-node-todo-app-cicd:v1
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 amitabhdevops/node-todo-app:v1
+docker run -d -p 8000:8000 amitabhdevops/aws-node-todo-app-cicd:v1
